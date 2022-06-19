678f76a4d12c27c78788db1213215966d1abd509
Сергей <tolltroll@yandex.ru>
2022-06-19 19:06:43 +0300

    5 страниц

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,8 +5,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.grabber.utils.DateTimeParser;
-import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 
@@ -17,9 +15,16 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
+        for (int i = 1; i < 6; i++) {
+            printPage(i);
+        }
+    }
 
-        DateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
-        Connection connection = Jsoup.connect(PAGE_LINK);
+    public static void printPage(int pageNumber) throws IOException {
+
+        System.out.println(String.format("----- Страница %d -------", pageNumber));
+
+        Connection connection = Jsoup.connect(PAGE_LINK + String.format("?page=%d", pageNumber));
         Document document = connection.get();
         Elements rows = document.select(".vacancy-card__inner");
         rows.forEach(row -> {
@@ -34,6 +39,5 @@ public class HabrCareerParse {
             String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
             System.out.printf("%s %s %s %n", vacancyName, link, dateTime);
         });
-
     }
 }
