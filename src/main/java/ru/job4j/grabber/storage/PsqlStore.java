151b321db44f4d352c66fe021e15fa35d8f82228
Сергей <tolltroll@yandex.ru>
2022-06-25 13:25:21 +0300

    PsqlStore

--- src/main/java/ru/job4j/grabber/storage/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/storage/PsqlStore.java
@@ -2,19 +2,22 @@ package ru.job4j.grabber.storage;
 
 import ru.job4j.grabber.entities.Post;
 
-import java.io.InputStream;
 import java.sql.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Properties;
 
 public class PsqlStore implements Store, AutoCloseable {
 
-    private static final String SAVE_QUERY = "INSERT INTO posts(name, description, link, created) VALUES (?,?,?,?);";
-    private static final String GET_ALL_QUERY = "SELECT * FROM posts;";
-    private static final String FIND_BY_ID = "SELECT * FROM posts WHERE id=? ;";
+    private static final String SAVE_QUERY = "INSERT INTO posts(name, link, description, created) VALUES (?,?,?,?)";
+    private static final String GET_ALL_QUERY = "SELECT * FROM posts";
+    private static final String FIND_BY_ID = "SELECT * FROM posts WHERE id=?";
+
+    private static final int ID_POS = 1;
+    private static final int NAME_POS = 2;
+    private static final int LINK_POS = 3;
+    private static final int DESCRIPTION_POS = 4;
+    private static final int CREATED_POS = 5;
 
     private Connection cnn;
 
@@ -35,8 +38,8 @@ public class PsqlStore implements Store, AutoCloseable {
     public void save(Post post) {
         try (PreparedStatement statement = cnn.prepareStatement(SAVE_QUERY)) {
             statement.setString(1, post.getTitle());
-            statement.setString(2, post.getDescription());
-            statement.setString(3, post.getLink());
+            statement.setString(2, post.getLink());
+            statement.setString(3, post.getDescription());
             statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
 
             statement.execute();
@@ -82,34 +85,17 @@ public class PsqlStore implements Store, AutoCloseable {
     }
 
     private void listFromResultSet(ResultSet resultSet, List<Post> resultList) throws SQLException {
-        if (Objects.isNull(resultSet) || Objects.isNull(resultList)) {
-            throw new IllegalArgumentException();
-        }
         while (resultSet.next()) {
             resultList.add(getPost(resultSet));
         }
     }
 
     private Post getPost(ResultSet resultSet) throws SQLException {
-        return new Post(resultSet.getInt(1),
-                resultSet.getString(2),
-                resultSet.getString(3),
-                resultSet.getString(4),
-                resultSet.getTimestamp(5).toLocalDateTime()
+        return new Post(resultSet.getInt(ID_POS),
+                resultSet.getString(NAME_POS),
+                resultSet.getString(LINK_POS),
+                resultSet.getString(DESCRIPTION_POS),
+                resultSet.getTimestamp(CREATED_POS).toLocalDateTime()
         );
-    }
-
-    public static void main(String... args) {
-        try (InputStream in = PsqlStore.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
-            Properties config = new Properties();
-            config.load(in);
-            Store store = new PsqlStore(config);
-            store.save(new Post("title", "description", "link " + LocalDateTime.now(), LocalDateTime.now()));
-            System.out.println(store.getAll());
-            System.out.println(store.findById(1));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
